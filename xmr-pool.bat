5e71e24504d27df1c1a14929715c694cc5be5458
anhphamngo <144450186+anhphamngo@users.noreply.github.com>
2023-10-12 22:30:00 +0700

    Add files via upload

---
+++ xmr-pool.bat
@@ -0,0 +1,3 @@
+@echo off
+xmrig.exe --donate-level 1 -o de.monero.herominers.com:1111 -u 474v87zFUz4HVskFsQY4kmhUmsLg4UNR1R7nSVa3L3ZdLwwKC3wdzwL6c44jUAB6b1XL45PRdmL5U2Tott3ycrhCEzAPuhE -p rigwindowten -a rx/0 -k
+pause
